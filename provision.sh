c7068c970f7a2ded945a3d27a568b5c7e95b4090
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-09 23:03:58 +0900

    update privisioning setting.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 nix-env -iA nixos.pkgs.haskellPlatform
-nix-env -iA nixos.pkgs.haskellPackages.cabalInstall_1_18_0_2
+nix-env -iA nixos.pkgs.haskellPackages.cabalInstall_1_18_0_3
 nix-env -iA nixos.pkgs.vim
+nix-env -iA nixos.pkgs.emacs
 nix-env -iA nixos.pkgs.apacheHttpd_2_4
